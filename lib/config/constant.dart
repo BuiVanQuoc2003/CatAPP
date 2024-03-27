8532b2742fdc894905155d2f625377158667492d
Ngoc My <mylam12022001@gmail.com>
2024-03-27 14:12:28 +0700

    repository

---
+++ lib/config/constant.dart
@@ -0,0 +1,1 @@
+const baseURL = "https://6603e67a2393662c31d008b6.mockapi.io/api/v1/cat/cats";
--- lib/feature/home/home_page.dart
+++ lib/feature/home/home_page.dart
@@ -1,10 +1,56 @@
+import 'package:catapp/feature/home/cat_card_widget.dart';
+import 'package:catapp/models/cat.dart';
+import 'package:catapp/repository/cat_repository.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  CatRepository catRepository = CatRepository();
+  List<Cat> list = [];
+
+  initData() async {
+    list = await catRepository.listCat();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    initData();
+
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+      body: Container(
+          height: size.height,
+          width: size.width,
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: GridView.builder(
+            itemCount: list.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              crossAxisCount: 2,
+              childAspectRatio: 0.5,
+            ),
+            itemBuilder: (context, index) {
+              return CatCardWidget(
+                cat: list[index],
+              );
+            },
+          )),
+    );
   }
 }
